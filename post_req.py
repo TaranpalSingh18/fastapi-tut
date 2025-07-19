523d15c8b065e627e0d69a0e61ef199d0c98dd59
Taranpal <taranarora1470@gmail.com>
2025-07-19 15:44:33 +0530

    Added the put and delete func in api

--- post_req.py
+++ post_req.py
@@ -2,17 +2,17 @@ from fastapi import FastAPI, HTTPException
 import json
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
-from typing import Annotated, List, Literal
+from typing import Annotated, Literal, Optional
+app=FastAPI()   
 
 class Patient(BaseModel):
     id: Annotated[str, Field(..., description="This is the id section of the patient", examples=["P001", "P004"])]
     name: Annotated[str, Field(max_length=50, description="The max length of the field could be 50 letters only", examples=["Taran Pal Singh, Zakir Khan"])]
     city: str
     age: Annotated[int, Field(ge=0, le=150, description="The range is 0-150", examples=['20', '100', '150'])]
-    gender: Annotated[Literal['male', 'female', 'others'], Field(..., description="This is the gender section of the patient class")]
-    height: Annotated[float, Field(..., description="This is the height class of the patient object", ge=0)]
-    weight: Annotated[float, Field(..., description="This is the weight class of the patient object", ge=0)]
-
+    gender: Annotated[Literal['male', 'female', 'others'], Field(description="This is the gender section of the patient class")]
+    height: Annotated[float, Field( description="This is the height class of the patient object", ge=0)]
+    weight: Annotated[float, Field( description="This is the weight class of the patient object", ge=0)]
 
     @computed_field()
     @property
@@ -20,8 +20,6 @@ class Patient(BaseModel):
     def bmi(self)->float:
         bmi = self.weight/self.height**2
         return bmi
-    
-
     @computed_field
     @property
     def condition(self)->str:
@@ -33,10 +31,7 @@ class Patient(BaseModel):
         
         if self.bmi>24.9:
             return "obese"
-
-
-app=FastAPI()   
-
+        
 def load_data():
     with open("patients.json", 'r') as f:
         data = json.load(f)
@@ -47,7 +42,14 @@ def save_data(data):
         data= json.dump(data, f)
        
 
-        
+class Patient_update(BaseModel):
+    id: Annotated[Optional[str], Field(default=None)]
+    name: Annotated[Optional[str], Field(default=None)]
+    city: Annotated[Optional[str], Field(default=None)]
+    age: Annotated[Optional[int], Field(default=None)]
+    gender: Annotated[Optional[Literal['male', 'female', 'others']], Field(default=None)]
+    height: Annotated[Optional[float], Field(description="This is the height class of the patient object", ge=0, default=None)]
+    weight: Annotated[Optional[float], Field(description="This is the weight class of the patient object", ge=0, default=None)]   
     
 @app.get('/view')
 def get_viewers():
@@ -66,9 +68,41 @@ def create_patient(object: Patient):
 
     JSONResponse(status_code=201, content={"message":"Patient Created Successfully"})
 
+@app.put('/edit/{patient_id}')
+def update_patient(patient_id: str, object: Patient_update):
+    data = load_data()
 
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail="The following patient id is not found")
+    
+    existing_patient_data = data[patient_id]
+    new_patient_data = object.model_dump(exclude_unset=True)
 
+    for key,value in new_patient_data.items():
+        existing_patient_data[key]=value
 
+    existing_patient_data['id']=patient_id
+
+    validated_patient_updated_data= Patient(**existing_patient_data)
+    #pydantic object into dictionary
+    updated_data = validated_patient_updated_data.model_dump(exclude='id')
+
+    data[patient_id]=updated_data
+    
+    save_data(data)
+
+    return JSONResponse(status_code=200, content="Successfully updated teh data")
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patiend_id:str):
+    data = load_data()
+    if patiend_id not in data:
+        raise HTTPException(status_code=404, detail="patient not found")
+    
+    del data[patiend_id]
+    save_data(data)
+
+    return JSONResponse(status_code=200, content="Patient deleted")
 
 
 
