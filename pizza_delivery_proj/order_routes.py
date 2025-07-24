e28304914c65f613259a34d2599659c8ed5d2bad
Taranpal <taranarora1470@gmail.com>
2025-07-24 11:47:08 +0530

    created a user order specific route and a list of user orders route

--- pizza_delivery_proj/order_routes.py
+++ pizza_delivery_proj/order_routes.py
@@ -54,20 +54,71 @@ async def placeOrder(payload: OrderModel, Authorise: AuthJWT = Depends()):
 #for super user only
 @order_router.get('/orders')
 async def get_all_orders(payload: OrderModel, Authorise: AuthJWT=Depends()):
-    session = Session(bind = engine)
     try:
         Authorise.jwt_required()
     except Exception as e:
         raise HTTPException(status_code=401, detail="Unauthorised access")
     
     current_user = Authorise.get_jwt_subject()
-    user = session.query(User).filter(User.username == current_user)
+    user = session.query(User).filter(User.username == current_user).first()
     if user.isStaff:
         all_orders = session.query(Order).all()
 
         return JSONResponse(status_code=200, content={"All Orders are": all_orders})
     
     raise HTTPException(status_code=404, detail="User Not Found/User is not a superuser")
+
+#for superuser only
+@order_router.get('/orders/{id}')
+async def get_order_by_id(id:int, Authorise: AuthJWT=Depends()):
+    try: 
+        Authorise.jwt_required()
+
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid Token")
+    
+    current_user = Authorise.get_jwt_subject()
+    user = session.query(User).filter(User.username==current_user).first()
+
+    if user.isStaff:
+        order=session.query(Order).filter(Order.id==id)
+        return JSONResponse(status_code=200, content={"Here's your order": order})
+
+    else:
+        return JSONResponse(status_code=401, content="Unauthorised access")
+    
+# for superuser only
+@order_router.get('/user/orders')
+async def get_all_orders_of_a_user(Authorise: AuthJWT=Depends()):
+    try:
+        Authorise.jwt_required()
+
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid Token")
+    
+    current_user = Authorise.get_jwt_subject()
+    user = session.query(User).filter(User.username==current_user)
+    #hence here we found our user
+    return JSONResponse(status_code=200, contnet={"message":user.orders})
+
+
+@order_router.get('/user/order/{order_id}')
+async def get_specific_order(order_id: int, Authorise:AuthJWT=Depends()):
+    session= Session(bind = engine)
+    try:
+        Authorise.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=404, detail="Invalid Token")
+    current_user = Authorise.get_jwt_subject
+    user = session.query(User).filter(User.username == current_user)
+    #my current user is user
+    order = user.orders
+
+    for i in order:
+        if order_id == i.id:
+            return i
+        
+    raise HTTPException(status_code=404,detail="No such order found")
 
 
 
